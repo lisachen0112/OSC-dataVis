b6de5529a05dc971129e154751b7c3e0d1d780b9
Lisa Chen <lisachen011299@gmail.com>
2024-06-16 14:43:14 +0200

    websocket osc signal to FE

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const oscModule = require('./public/osc')
+const server = require('http').createServer(app);
+const WebSocket = require('ws');
+const osc = require('osc');
+
 const port = 8080
 
 app.set('view engine', 'ejs')
@@ -9,6 +14,33 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+// Create WebSocket server
+const wss = new WebSocket.Server({ server:server });
+
+wss.on('connection', function connection(ws) {
+    console.log('Client connected');
+    ws.send('Server sees client connected');
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    
+});
+
+oscModule.on("ready", () => {
+    server.listen(port, () => {
+        console.log(`Listening on port ${port}`)
+    });
+});
+
+oscModule.on("message", (oscMsg, timeTag, info) => {
+    console.log("Received OSC message:");
+    console.log("Address: ", oscMsg.address); // The OSC address pattern (e.g., "/fader1")
+    console.log("Arguments: ", oscMsg.args);   // Array of arguments sent with the message
+    console.log("Remote info: ", info);         // Information about the sender (IP, port)
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(oscMsg));
+        }
+    });
+});
--- public/osc.js
+++ public/osc.js
@@ -7,15 +7,13 @@ const udpPort = new osc.UDPPort({
 });
 
 udpPort.open();
-console.log("Listening for OSC messages on port 9000...");
 
-udpPort.on("message", function(oscMsg, timeTag, info) {
-    console.log("Received OSC message:");
-    console.log("Address: ", oscMsg.address); // The OSC address pattern (e.g., "/fader1")
-    console.log("Arguments: ", oscMsg.args);   // Array of arguments sent with the message
-    console.log("Remote info: ", info);         // Information about the sender (IP, port)
+udpPort.on("ready", function() {
+    console.log("OSC UDP Port is ready!");
 });
 
 udpPort.on("error", function(error) {
     console.error("An error occurred:", error.message);
 });
+
+module.exports = udpPort;
