32df7496dd240abf9f5b3e4ca3d5b3b49fd6413b
Lisa Chen <lisachen011299@gmail.com>
2024-09-05 20:06:44 +0100

    financial data

--- App.js
+++ App.js
@@ -4,6 +4,56 @@ const oscModule = require('./public/osc')
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
 const osc = require('osc');
+// const fs = require('fs');
+// const path = require('path');
+// const yahooFinance = require('yahoo-finance2').default;
+// const moment = require('moment');
+
+// async function fetchHistoricalStockPrices() {
+//     const tickers = ['AMZN', 'AAPL', 'TSLA', 'NFLX'];
+//     const endDate = new Date();
+//     const startDate = moment().subtract(6, 'months').toDate();
+
+//     const historicalData = {};
+
+//     for (const ticker of tickers) {
+//         try {
+//             // Fetch historical stock data
+//             const data = await yahooFinance.historical(ticker, {
+//                 period1: startDate,
+//                 period2: endDate,
+//                 interval: '1d'
+//             });
+
+//             historicalData[ticker] = data;
+//         } catch (error) {
+//             console.error(`Error fetching historical data for ${ticker}:`, error);
+//         }
+//     }
+
+//     // Define the JSON content
+//     const jsonContent = JSON.stringify(historicalData, null, 2);
+
+//     // Define the path
+//     const dir = 'public/data';
+//     const filePath = path.join(dir, 'historical_stock_prices.json');
+
+//     // Ensure the directory exists
+//     if (!fs.existsSync(dir)) {
+//         fs.mkdirSync(dir, { recursive: true });
+//     }
+
+//     // Write the JSON content to a file
+//     fs.writeFile(filePath, jsonContent, (err) => {
+//         if (err) {
+//             console.error('Error writing file:', err);
+//         } else {
+//             console.log('Historical stock prices saved successfully in public/data');
+//         }
+//     });
+// }
+
+// fetchHistoricalStockPrices();
 
 const port = 8080
 
@@ -19,13 +69,13 @@ const wss = new WebSocket.Server({ server:server });
 
 wss.on('connection', function connection(ws) {
     console.log('Client connected');
-    // ws.send('Server sees client connected');
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
     
 });
+
 
 oscModule.on("ready", () => {
     server.listen(port, () => {
---
+++ public/interaction.js
@@ -0,0 +1,154 @@
+const socket = new WebSocket('ws://localhost:8080');
+
+var mode = 0; // default mode 0
+
+socket.addEventListener('open', function (event) {
+  console.log('Client connects to WS server');
+});
+
+// Errors
+socket.addEventListener('error', function (event) {
+  console.error('WebSocket error observed:', event);
+});
+
+// Listen for messages from the server
+socket.addEventListener('message', function (event) {
+
+    var data = JSON.parse(event.data);
+    console.log(data);
+    // if (data.type === 'stockData') { 
+    //     console.log(data);
+    // }
+
+    if (data.address == "/modeRadio") {
+        mode = data.args;
+        console.log("Changed mode to " + mode);
+    }
+
+
+});
+
+
+fetch('charts/bar.json')
+.then(response => response.json())
+.then(spec => {
+    vegaEmbed('#barChart', spec).then(({ spec, view }) => {
+      var isHovering = false; 
+  
+      view.addEventListener('mouseover', () => {isHovering = true;});
+      view.addEventListener('mouseout', () => {isHovering = false;});
+  
+      function select(newIndex) {
+          view.signal("selectIndex", newIndex).run()
+      }
+  
+      function tooltip(show) {
+          view.signal("tooltip", show == 1).run();
+      }
+  
+      function zoom(newZoom) {
+          view.signal("zoom", newZoom).run();
+      }
+  
+      function OSCtoCommand(oscMsg) {
+          if (!isHovering) {
+              return;
+          }
+  
+          var data = JSON.parse(oscMsg);
+          console.log(data);
+      
+          if (data.address == "/fader1") {
+              if (mode == 0) {
+                  select(data.args);
+              }
+              else if (mode == 1){
+                  zoom(data.args);
+              }
+          }
+      
+          if (data.address == "/button1") {
+              if (mode == 0) {
+                  tooltip(data.args);
+              }
+              else if (mode == 1) {
+                  zoom(0.5);
+              }
+              else {
+                  console.log("Command not found")
+              }
+          }
+      }
+  
+      // Listen for messages from the server
+      socket.addEventListener('message', function (event) {
+          OSCtoCommand(event.data);
+      });
+  
+  }).catch(console.error); 
+});
+
+
+fetch('charts/scatter.json')
+.then(response => response.json())
+.then(spec => {
+    vegaEmbed('#scatterPlot', spec).then(({ spec, view }) => {
+      var isHovering = false; 
+  
+      view.addEventListener('mouseover', () => {isHovering = true;});
+      view.addEventListener('mouseout', () => {isHovering = false;});
+
+      function zoom(newZoom) {
+        view.signal("zoom", parseFloat(newZoom) + 0.5).run();
+        console.log("zooming with " + parseFloat(newZoom) + 0.5);
+      }
+
+      function OSCtoCommand(oscMsg) {
+        if (!isHovering) {
+            return;
+        }
+
+        var data = JSON.parse(oscMsg);
+        console.log(data);
+    
+        if (data.address == "/fader1") {
+            if (mode == 0) {
+                select(data.args);
+            }
+            else if (mode == 1){
+                zoom(data.args);
+            }
+        }
+    
+        if (data.address == "/button1") {
+            if (mode == 0) {
+                tooltip(data.args);
+            }
+            else if (mode == 1) {
+                zoom(0.5);
+            }
+            else {
+                console.log("Command not found")
+            }
+        }
+      }
+
+      // Listen for messages from the server
+      socket.addEventListener('message', function (event) {
+          OSCtoCommand(event.data);
+      });
+
+    }).catch(console.error);
+});
+
+fetch('charts/cross-original.json')
+.then(response => response.json())
+.then(spec => {
+    vegaEmbed('#crossBarChart', spec)
+        .catch(console.error);
+});
+
+
+
+  
+
