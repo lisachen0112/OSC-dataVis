209f357595fbf66e119744b974d138865a66fc00
Lisa Chen <lisachen011299@gmail.com>
2024-09-23 22:56:50 +0100

    select addon lines

--- public/interaction.js
+++ public/interaction.js
@@ -3,7 +3,9 @@ const socket = new WebSocket('ws://localhost:8080');
 var mode = 0; // default mode 0
 var activeTicker = 0;
 var tickers = ['AMZN', 'AAPL', 'NFLX', 'TSLA']
-var scroll_val = 0;
+var prevStepValue  = 0;
+var prevStepValueAddOn = 0;
+var addOnActive = 0;
 
 socket.addEventListener('open', function (event) {
   console.log('Client connects to WS server');
@@ -32,7 +34,7 @@ socket.addEventListener('message', function (event) {
       closeSearch();
     }
 
-    if (data.address == "/scroll" && mode == 0) {
+    if (data.address == "/scroll") {
       scroll(data.args);
     }
 
@@ -49,40 +51,68 @@ function scroll(val) {
   if (mode == 0) {
     if (document.getElementById('tickerSelect').style.display == 'flex') {
       var option = document.getElementById('options-container');
+      var currentStep;
 
-      const threshold = 0.5;  // Arbitrary midpoint value to detect wrap-around
-      let delta = val- scroll_val;
-
-      if (scroll_val > threshold && val< threshold) {
-        delta = (val + 1) - scroll_val;
-      } else if (scroll_val < threshold && val > threshold) {
-        delta = val - (scroll_val+ 1);  
+      if (val >= 0 && val < 0.25) {
+        currentStep = 0;
+      }
+      else if (val >= 0.25 && val < 0.5) {
+        currentStep = 0.25;
+      }
+      else if (val >= 0.5 && val < 0.75) {
+        currentStep = 0.5;
+      }
+      else if (val >= 0.75 && val <= 1) {
+        currentStep = 0.75;
       }
 
-      scroll_val = val;
-
-      if (delta > 0) {
-          activeTicker++;
-          if (activeTicker > option.children.length - 1) {
-            activeTicker = 0;
-          }
-          console.log(activeTicker);
-          option.children[activeTicker].focus();
+      if (currentStep > prevStepValue || (currentStep == 0 && prevStepValue == 0.75)) {
+        activeTicker++;
+        if (activeTicker > option.children.length - 1) {
+          activeTicker = 0;
         }
-      else if (delta < 0) {
+      } else if (currentStep < prevStepValue || (currentStep == 0.75 && prevStepValue == 0)) {
         activeTicker--;
         if (activeTicker < 0) {
           activeTicker = option.children.length - 1;
         }
-        console.log(activeTicker);
-        option.children[activeTicker].focus();
       }
+      option.children[activeTicker].focus();
+      prevStepValue = currentStep;
     }
   }
   else if (mode == 1) {
-    if (document.getElementById('tickerSelect').style.display == 'flex') {}
+    if (document.getElementById('addOns').classList.contains('show')) {
+      var currentStep;
+
+      if (val >= 0 && val < 0.25) {
+        currentStep = 0;
+      }
+      else if (val >= 0.25 && val < 0.5) {
+        currentStep = 0.25;
+      }
+      else if (val >= 0.5 && val < 0.75) {
+        currentStep = 0.5;
+      }
+      else if (val >= 0.75 && val <= 1) {
+        currentStep = 0.75;
+      }
+
+      if (currentStep > prevStepValueAddOn || (currentStep == 0 && prevStepValueAddOn == 0.75)) {
+        addOnActive++;
+        if (addOnActive > 3) {
+          addOnActive = 0;
+        }
+      } else if (currentStep < prevStepValueAddOn || (currentStep == 0.75 && prevStepValueAddOn == 0)) {
+        addOnActive--;
+        if (addOnActive < 0) {
+          addOnActive = 2;
+        }
+      }
+      document.querySelectorAll('.item')[addOnActive].focus();
+      prevStepValueAddOn = currentStep;
+    }
   }
-  
 }
 
 function closeSearch() {
@@ -98,6 +128,7 @@ function closeSearch() {
   else if (mode == 1) {
     const addon = document.getElementById('addOns');
     addon.classList.remove('show');
+    addOnActive = 0;
   }
 }
 
@@ -111,9 +142,6 @@ function search() {
   else if (mode == 1) {
     const addon = document.getElementById('addOns');
     addon.classList.add('show');
-    const item = document.querySelectorAll('.item')[0];
-    console.log(item);
-    item.focus();
   }
 }
 
@@ -178,29 +206,6 @@ fetch('charts/stock-vega-lite.json')
 .then(spec => {
     vegaEmbed('#barChart', spec).then(({spec, view}) => {
 
-        // function minBound(val) {
-        //     var currRange = spec.vconcat[1].params[0].value.x;
-        //     currRange = [currRange[0] - val, currRange[1]];
-
-        //     console.log(spec);
-
-        //     // Render the updated spec
-        //     vegaEmbed('#barChart', spec).then(({spec, view}) => {
-        //         chartView = view;
-        //     }).catch(console.error);
-        // }
-
-        // function minBound(val) {
-        //     // Define the new bounds for the brush
-        //     var newBounds = [1719596800000, view.signal('brush').date[1]];
-        //     console.log(newBounds);
-        //     // Update the brush signal with new bounds
-        //     view.signal('brush', {"date": newBounds}).run();
-            
-        //     // Notify Vega-Lite to update the visualization
-        //     // view.runAsync().catch(console.error);
-        // }
-
         function OSCtoCommand(oscMsg) {
     
             var data = JSON.parse(oscMsg);
@@ -233,21 +238,78 @@ fetch('charts/stock-vega-lite.json')
         document.getElementById('tickerSelect').addEventListener('click', function() {
           spec.transform[0].filter = "datum.ticker === '" + tickers[activeTicker] + "'";
 
-          console.log(spec.transform[0].filter);
-
           // Render the updated spec
           vegaEmbed('#barChart', spec).then(({spec, view}) => {
             chartView = view;
           }).catch(console.error);
         });
         
-
         // Event listener to add reference lines
         const items = document.querySelectorAll('.item');
         items.forEach(item => {
-          item.addEventListener('click', event => {
-            item.classList.toggle('checked');
-            // change spec and rerender
+          item.addEventListener('keypress', event => {
+            if (event.key === 'Enter') {
+              item.classList.toggle('checked');
+              // change spec and rerender
+              if (item.classList.contains('checked')) {
+                if (item.id == '7days') {
+                  spec.vconcat[0].layer.push({
+                    "mark": "line",
+                    "encoding": {
+                      "y": {"field": "MVGA7", "type": "quantitative"},
+                      "color": {"value": "red"},
+                      "opacity": {"value": 0.5}
+                    }
+                  })
+                }
+
+                if (item.id == '30days') {
+                  spec.vconcat[0].layer.push({
+                    "mark": "line",
+                    "encoding": {
+                      "y": {"field": "MVGA30", "type": "quantitative"},
+                      "color": {"value": "orange"},
+                      "opacity": {"value": 0.5}
+                    }
+                  })
+                }
+
+                // Render the updated spec
+                vegaEmbed('#barChart', spec).then(({spec, view}) => {
+                  chartView = view;
+                }).catch(console.error);
+              }
+              else {
+                if (item.id == '7days') {
+                  var i = 1;
+                  while (i < spec.vconcat[0].layer.length) {
+                    if (spec.vconcat[0].layer[i].encoding.y.field == 'MVGA7') {
+                      spec.vconcat[0].layer.splice(i, 1);
+                    }
+                    i++;
+                  }
+                }
+
+                if (item.id == '30days') {
+                  var i = 1;
+                  while (i < spec.vconcat[0].layer.length) {
+                    if (spec.vconcat[0].layer[i].encoding.y.field == 'MVGA30') {
+                      spec.vconcat[0].layer.splice(i, 1);
+                    }
+                    i++;
+                  }
+                  
+                }
+
+                // Render the updated spec
+                vegaEmbed('#barChart', spec).then(({spec, view}) => {
+                  chartView = view;
+                }).catch(console.error);
+
+              }
+ 
+              
+            }
           });
         });
         
